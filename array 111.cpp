c3a9e2cfd1bf6acc92eb12a27b2ff32c3908b433
Gebre_Aw <gebremikaelaweta4@gmail.com>
2025-01-14 20:48:36 +0000

    Update array 111.cpp
    I added product by using array

--- array 111.cpp
+++ array 111.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-int sum[5]={20,40,5,6,45};
+int num[5]={20,40,5,6,45};
 int sum = 0;
+    int product =1;
 for (int i=0;i<5;i++){
     sum=sum+add[i];
 
 }
 cout<<sum[i]<<endl;
+    for (int i=0;i<5;i++){
+    product=product*num[i];
+
+}
+cout<<product[i]<<endl;
 return 0;
 }
